de7c90a80f8578053c79e62b57e51a02c66effe2
Kid <44045911+kidonng@users.noreply.github.com>
2022-08-26 21:35:31 +0000

    Simplify

--- index.ts
+++ index.ts
@@ -2,29 +2,28 @@ import type {Preset} from 'unocss'
 import camelCase from 'camelcase'
 import colors from 'daisyui/src/colors/index.js'
 
-const baseShades = [100, 200, 300]
+const colorEntries = Object.entries(colors)
 
 const presetDaisy = (): Preset => ({
 	name: 'daisy',
 	theme: {
 		colors: {
 			...Object.fromEntries(
-				Object.entries(colors)
+				colorEntries
 					.filter(
 						([color]) =>
-							![
-								// Already in preset-mini
-								// https://github.com/unocss/unocss/blob/0f7efcba592e71d81fbb295332b27e6894a0b4fa/packages/preset-mini/src/_theme/colors.ts#L11-L12
-								'transparent',
-								'current',
-								// Added below
-								...baseShades.map((shade) => `base-${shade}`),
-							].includes(color),
+							// Already in preset-mini
+							// https://github.com/unocss/unocss/blob/0f7efcba592e71d81fbb295332b27e6894a0b4fa/packages/preset-mini/src/_theme/colors.ts#L11-L12
+							!['transparent', 'current'].includes(color) &&
+							// Added below
+							!color.startsWith('base'),
 					)
 					.map(([color, value]) => [camelCase(color), value({})]),
 			),
 			base: Object.fromEntries(
-				baseShades.map((shade) => [shade, colors[`base-${shade}`]!({})]),
+				colorEntries
+					.filter(([color]) => color.startsWith('base'))
+					.map(([color, value]) => [color.replace('base-', ''), value({})]),
 			),
 		},
 	},
